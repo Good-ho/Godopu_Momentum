668a393e846a251afd84c09462aeceb94b9f9fe9
Good-ho <whdxo100@gmail.com>
2020-10-12 20:48:01 +0900

    Making a To Do List part two
    	1. 이번 강의 핵심은 localStorage는 only string만 저장할 수 있다는 것.
    	따라서 js data는 저장이 안됨.
    	
    Object를 저장하기 위해서는 json.strinify를 통해 string으로 저장하고 그것을 parse할 때 해당 object로 꺼낼 필요 있음.

--- todo.js
+++ todo.js
@@ -4,25 +4,52 @@ const toDoList = document.querySelector(".js-toDoList");
 
 const TODOS_LS = "toDo";
 
+const toDos = [];
+
 function loadTodo(){
-    const toDo = localStorage.getItem(TODOS_LS);
-    if(toDo === null) {
+    const loadTodo = localStorage.getItem(TODOS_LS);
+    if(loadTodo === null) {
 
     } else {
-
+        // 아래 log 출력 결과를 보면 loadTodo는 string으로 출력되고
+        // parse는 object로 출력되는 것을 확인 할 수 있다.
+        // console.log(loadTodo);
+        const parse = JSON.parse(loadTodo);
+        // console.log(parse);
+        parse.forEach(function(ele){
+            paintTodo(ele.text);
+        })
     }
+}
+
+function saveToDos(){
+    //localStorage에는 js data를 저장할 수 없다.
+    // 따라서 아래와 같이 작성하고 출력해보면 localStorage에 object, object로 저장됨.
+    // LocalStorage에는 only string만 저장가능.
+    // localStorage.setItem(TODOS_LS, toDos);
+    // 따라서 아래와 같이 json으로 저장해야함.
+    // json.stringify 는 js data를 string으로 저장함.
+    localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
 }
 
 function paintTodo(text){
     // create list
     const li = document.createElement("li");
     const delBtn = document.createElement("button");
-    delBtn.innerText = "x";
     const span = document.createElement("span");
+    const newId = toDos.length + 1;
+    delBtn.innerText = "x";
     span.innerText = text;
-    li.appendChild(delBtn);
+    li.appendChild(delBtn); 
     li.appendChild(span);
+    li.id = newId;
     toDoList.appendChild(li);
+    const toDoObj = {
+        text : text,
+        id: newId
+    };
+    toDos.push(toDoObj);
+    saveToDos();
 }
 
 function handleSummit(event){
